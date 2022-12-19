23e5a5558c278c04a969d7fce4c4b76b61572f1a
chay <lichenyi_2020@qq.com>
2022-12-19 12:07:21 +0800

    完成menu()函数

--- 作品.py
+++ 作品.py
@@ -1,1 +1,11 @@
-
+import random,math
+def menu():
+	print("------多功能一体机------")
+	print("功能：")
+	print("-----------------------")
+	print("序号          功能")
+	print("1            抽取随机数")
+	print("2            图形计算器")
+	print("3            多功能计算器")
+	print("4            math库计算器")
+	print("5            退出")
